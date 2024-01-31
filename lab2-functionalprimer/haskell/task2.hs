9fe49cd130802a2f17a2f2ab118ab6c974d7ebb2
Rahul Vasantlal <220344548@aston.ac.uk>
2024-01-31 12:51:16 +0000

    @

--- lab2-functionalprimer/haskell/task2.hs
+++ lab2-functionalprimer/haskell/task2.hs
@@ -1,4 +1,12 @@
+
+onePlusone :: Int -> Int
+onePlusone x = x + 1
+
+main :: IO ()
 main = do
     putStrLn "Welcome to the programme. Please enter your name"
     name <- getLine
     putStrLn("Hello " ++ name ++ ", hope you like Haskell.")
+    let result = onePlusone 1 
+    putStrLn $ "1 + 1 = " ++ show result
+    
